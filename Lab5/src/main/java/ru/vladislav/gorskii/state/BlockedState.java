9f6e8db0a21f9d8a373fa05ad78672ae7e4e85a7
VladTheMountain <vg07102004@gmail.com>
2025-01-22 12:50:23 +0300

    Initial commit

---
+++ Lab5/src/main/java/ru/vladislav/gorskii/state/BlockedState.java
@@ -0,0 +1,31 @@
+package ru.vladislav.gorskii.state;
+
+public class BlockedState implements IState{
+
+    private final ATM machine;
+
+    public BlockedState(ATM instance) {
+        this.machine = instance;
+    }
+
+    @Override
+    public String enterPIN() {
+        return "Failed: Machine has been blocked";
+    }
+
+    @Override
+    public String withdraw(int num) {
+        return "Failed: Machine has been blocked";
+    }
+
+    @Override
+    public String terminate() {
+        this.machine.setState(new IdleState(this.machine));
+        return "Session terminated";
+    }
+
+    @Override
+    public String deposit(int num) {
+        return "Failed: Machine has been blocked";
+    }
+}
---
+++ Lab5/src/main/java/ru/vladislav/gorskii/state/ProcessingState.java
@@ -0,0 +1,32 @@
+package ru.vladislav.gorskii.state;
+
+public class ProcessingState implements IState{
+
+    private final ATM machine;
+    private boolean isWithdrawal;
+
+    public ProcessingState(ATM instance, int amountChange) {
+        this.machine = instance;
+        this.isWithdrawal = amountChange < 0;
+    }
+
+    @Override
+    public String enterPIN() {
+        return "Failed: Currently busy";
+    }
+
+    @Override
+    public String withdraw(int num) {
+        return this.isWithdrawal ? "Failed: Already withdrawing..." : "Failed: Currently busy";
+    }
+
+    @Override
+    public String terminate() {
+        return "Failed: Can't abort right now, please wait.";
+    }
+
+    @Override
+    public String deposit(int num) {
+        return this.isWithdrawal ? "Failed: Currently busy" : "Failed: Already depositing...";
+    }
+}
